d5077d91bf13dc3ff45b64ac14431e577e8ca011
mttstt <mtt_stt@yahoo.it>
2020-03-19 23:52:25 +0100

    Implemented WAIT variable

--- EspHome/aurora.h
+++ EspHome/aurora.h
@@ -2,6 +2,7 @@
 #include "ABBAurora.h"
 #include "ABBAuroraStrings.h"
 
+#define WAIT 50
 #define RX1 3			// GPIO-3
 #define TX1 1			// GPIO-1
 #define INVERTER_ADDRESS 2
@@ -37,37 +38,34 @@ class AuroraTextSensor : public PollingComponent, public TextSensor {
     ESP_LOGD("Aurora", "Init AuroraTextSensor update data");
     char buf[80];
     char bof[80];
+    delay(WAIT);
     if ( inverter->ReadState() )
       {
-        delay(100);
         buf[0] = '\0';str->TransmissionState(inverter->State.TransmissionState).toCharArray(buf, sizeof(buf));
         TransmissionState_sensor->publish_state(buf);
 
-        delay(100);
         buf[0] = '\0';str->GlobalState(inverter->State.GlobalState).toCharArray(buf, sizeof(buf));
         GlobalState_sensor->publish_state(buf);
 
-        delay(100);
         buf[0] = '\0';str->InverterState(inverter->State.InverterState).toCharArray(buf, sizeof(buf));
         InverterState_sensor->publish_state(buf);
 
-        delay(100);
         buf[0] = '\0';str->AlarmState(inverter->State.AlarmState).toCharArray(buf, sizeof(buf));
         AlarmState_sensor->publish_state(buf);
       }
-    delay(100);
+    delay(WAIT);
     if ( inverter->ReadFirmwareRelease() )
       {
         buf[0] = '\0';(inverter->FirmwareRelease.Release).toCharArray(buf, sizeof(buf));
         Firmware_sensor->publish_state(buf);
       }
-    delay(100);
+    delay(WAIT);
     if ( inverter->ReadSystemSerialNumber() )
         {
           buf[0] = '\0';(inverter->SystemSerialNumber.SerialNumber).toCharArray(buf, sizeof(buf));
           SystemSerialNumber_sensor->publish_state(buf);
         }
-    delay(100);
+    delay(WAIT);
     if ( inverter->ReadManufacturingWeekYear() )
         {
           bof[0] = '\0';(inverter->ManufacturingWeekYear.Week).toCharArray(bof, sizeof(bof));
@@ -75,7 +73,7 @@ class AuroraTextSensor : public PollingComponent, public TextSensor {
           strcat(buf, ", "); strcat(buf, bof);
           ManufacturingWeekYear_sensor->publish_state(buf);
         }
-    delay(100);
+    delay(WAIT);
     if ( inverter->ReadLastFourAlarms() )
         {
           buf[0] = '\0';
@@ -89,7 +87,7 @@ class AuroraTextSensor : public PollingComponent, public TextSensor {
           strcat(buf, bof);
           LastFourAlarms_sensor->publish_state(buf);
         }
-    delay(100);
+    delay(WAIT);
     if ( inverter->ReadVersion() )
         {
           buf[0] = '\0';
@@ -109,111 +107,111 @@ class AuroraTextSensor : public PollingComponent, public TextSensor {
 
 
 
-  class AuroraSensor : public PollingComponent {
-  public:
-  ABBAurora *inverter;
+class AuroraSensor : public PollingComponent {
+public:
+ABBAurora *inverter;
 
-  Sensor *PowerIn1_sensor = new Sensor();
-  Sensor *PowerIn2_sensor = new Sensor();
-  Sensor *GridPower_sensor = new Sensor();
-  Sensor *TemperatureInverter_sensor = new Sensor();
-  Sensor *TemperatureBooster_sensor = new Sensor();
-  Sensor *DayCumulatedEnergy_sensor = new Sensor();
-  Sensor *MonthCumulatedEnergy_sensor = new Sensor();
-  Sensor *YearCumulatedEnergy_sensor = new Sensor();
-  Sensor *VIn1_sensor = new Sensor();
-  Sensor *IIn1_sensor = new Sensor();
-  Sensor *VIn2_sensor = new Sensor();
-  Sensor *IIn2_sensor = new Sensor();
-  Sensor *IsolationResistance_sensor = new Sensor();
-  Sensor *PowerPeak_sensor = new Sensor();
-  Sensor *PowerPeakToday_sensor = new Sensor();
+Sensor *PowerIn1_sensor = new Sensor();
+Sensor *PowerIn2_sensor = new Sensor();
+Sensor *GridPower_sensor = new Sensor();
+Sensor *TemperatureInverter_sensor = new Sensor();
+Sensor *TemperatureBooster_sensor = new Sensor();
+Sensor *DayCumulatedEnergy_sensor = new Sensor();
+Sensor *MonthCumulatedEnergy_sensor = new Sensor();
+Sensor *YearCumulatedEnergy_sensor = new Sensor();
+Sensor *VIn1_sensor = new Sensor();
+Sensor *IIn1_sensor = new Sensor();
+Sensor *VIn2_sensor = new Sensor();
+Sensor *IIn2_sensor = new Sensor();
+Sensor *IsolationResistance_sensor = new Sensor();
+Sensor *PowerPeak_sensor = new Sensor();
+Sensor *PowerPeakToday_sensor = new Sensor();
 
-  AuroraSensor() : PollingComponent(300000) { }
+AuroraSensor() : PollingComponent(300000) { }
 
 
-  void setup() override
+void setup() override
   {
-    inverter = new ABBAurora(INVERTER_ADDRESS);
+   inverter = new ABBAurora(INVERTER_ADDRESS);
   }
 
-  void update() override
+void update() override
   {
     ESP_LOGD("Aurora", "Init AuroraSensor update data");
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(POWER_IN_1, MODULE_MESSUREMENT))
        {
-         PowerIn1_sensor->publish_state(inverter->DSP.Value);
+        PowerIn1_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(POWER_IN_2, MODULE_MESSUREMENT))
        {
-         PowerIn2_sensor->publish_state(inverter->DSP.Value);
+        PowerIn2_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(GRID_POWER, MODULE_MESSUREMENT))
        {
-       	 GridPower_sensor->publish_state(inverter->DSP.Value);
+       	GridPower_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(TEMPERATURE_INVERTER, MODULE_MESSUREMENT))
        {
-         TemperatureInverter_sensor->publish_state(inverter->DSP.Value);
+        TemperatureInverter_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(TEMPERATURE_BOOSTER, MODULE_MESSUREMENT))
        {
-       	 TemperatureBooster_sensor->publish_state(inverter->DSP.Value);
+       	TemperatureBooster_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadCumulatedEnergy(CURRENT_DAY))
        {
-      	 DayCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+      	DayCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadCumulatedEnergy(CURRENT_MONTH))
        {
-         MonthCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+        MonthCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadCumulatedEnergy(CURRENT_YEAR))
        {
-         YearCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
+        YearCumulatedEnergy_sensor->publish_state(inverter->CumulatedEnergy.Energy);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(V_IN_1, GLOBAL_MESSUREMENT))
        {
-         VIn1_sensor->publish_state(inverter->DSP.Value);
+        VIn1_sensor->publish_state(inverter->DSP.Value);
        }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(I_IN_1, GLOBAL_MESSUREMENT))
        {
-          IIn1_sensor->publish_state(inverter->DSP.Value);
+        IIn1_sensor->publish_state(inverter->DSP.Value);
         }
-    delay(100);
+    delay(WAIT);
     if (inverter->ReadDSPValue(V_IN_2, GLOBAL_MESSUREMENT))
-        {
-          VIn2_sensor->publish_state(inverter->DSP.Value);
-         }
+       {
+        VIn2_sensor->publish_state(inverter->DSP.Value);
+       }
     delay(100);
     if (inverter->ReadDSPValue(I_IN_2, GLOBAL_MESSUREMENT))
-        {
-          IIn2_sensor->publish_state(inverter->DSP.Value);
-        }
-    delay(100);
+       {
+        IIn2_sensor->publish_state(inverter->DSP.Value);
+       }
+    delay(WAIT);
     if (inverter->ReadDSPValue(ISOLATION_RESISTANCE, MODULE_MESSUREMENT))
-         {
-           IsolationResistance_sensor->publish_state(inverter->DSP.Value);
-         }
-    delay(100);
+       {
+        IsolationResistance_sensor->publish_state(inverter->DSP.Value);
+       }
+    delay(WAIT);
     if (inverter->ReadDSPValue(POWER_PEAK, MODULE_MESSUREMENT))
-        {
-          PowerPeak_sensor->publish_state(inverter->DSP.Value);
-        }
-    delay(100);
+       {
+        PowerPeak_sensor->publish_state(inverter->DSP.Value);
+       }
+    delay(WAIT);
     if (inverter->ReadDSPValue(POWER_PEAK_TODAY, MODULE_MESSUREMENT))
-        {
-          PowerPeakToday_sensor->publish_state(inverter->DSP.Value);
-        }
+       {
+        PowerPeakToday_sensor->publish_state(inverter->DSP.Value);
+       }
     }
-  };
+};
